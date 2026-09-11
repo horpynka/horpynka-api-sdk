d611cc09ca91080cdedd2331f28bc4d37a6e30bf
Artem Bohak <artem.bohak@hotline.finance>
2026-09-12 00:13:11 +0300

    added new api

--- src/client/clients.ts
+++ src/client/clients.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosInstance, CreateAxiosDefaults } from "axios";
-import { Configuration, OrdersApi } from "../api/generated";
+import { AuthApi, Configuration, OrdersApi } from "../api/generated";
 import { QueryClient } from "@tanstack/react-query";
 
 export let tanstackClient: QueryClient;
 export let ordersApi: OrdersApi;
+export let authApi: AuthApi;
 
 export function initApiClient(config: CreateAxiosDefaults): AxiosInstance {
   const apiClient = axios.create({
@@ -17,6 +18,7 @@ export function initApiClient(config: CreateAxiosDefaults): AxiosInstance {
 
   tanstackClient = new QueryClient();
   ordersApi = new OrdersApi(configuration, undefined, apiClient);
+  authApi = new AuthApi(configuration, undefined, apiClient);
 
   return apiClient;
 }
